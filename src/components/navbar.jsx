7943527c97de466a4e9b5a32dc9b17e63362cbb2
khaan <nguyenkhaan2006@gmail.com>
2025-11-13 09:09:33 +0700

    sua_file_index

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+function Navbar()
+{
+    return (
+        <div className='w-screen fixed h-80 bg-blue-500'>
+
+        </div>
+    )
+}
+export default Navbar
---
+++ src/layouts/LandingPageLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Navbar from '../components/navbar'
+function LandingPageLayout()
+{
+    return (
+        <div className='w-full bg-yellow-500 min-h-80'>
+            
+        </div>
+    )
+}
+export default LandingPageLayout
